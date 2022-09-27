540543c687cbecd0c0d2b5723eaab3efe85e8b82
ACER <ACER@DESKTOP-E6IT92A>
2022-09-27 16:01:00 +0530

    updated..

---
+++ src/test/java/Githubpackage/UpdateTest.java
@@ -0,0 +1,12 @@
+package Githubpackage;
+
+import org.testng.annotations.Test;
+
+public class UpdateTest {
+	@Test
+	public void update() {
+		System.out.println("--sheeba--");
+		
+	}
+
+}
--- src/test/java/Githubpackage/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author hp user
- *
- */
-package Githubpackage;
